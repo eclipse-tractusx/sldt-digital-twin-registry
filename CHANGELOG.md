a8a4474b1134916fab9b7f6c0f64d6ea185b64eb
Aggarwal Sahil (BCI/ESW17) <Sahil.Aggarwal@de.bosch.com>
2023-07-12 14:20:31 +0200

    Temporary fix done for getting SemanticID in Submodel response

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 0.3.8-M1
+### Added
+
+## fixed
+- In this version, fix is given for adjusting SemanticID in submodel response. This is only temporary workaround.
+
 ## 0.3.7-M1
 ### Added
 
--- backend/src/main/java/org/eclipse/tractusx/semantics/registry/mapper/SubmodelMapper.java
+++ backend/src/main/java/org/eclipse/tractusx/semantics/registry/mapper/SubmodelMapper.java
@@ -23,6 +23,7 @@ package org.eclipse.tractusx.semantics.registry.mapper;
 
 import org.eclipse.tractusx.semantics.aas.registry.model.Endpoint;
 import org.eclipse.tractusx.semantics.aas.registry.model.Key;
+import org.eclipse.tractusx.semantics.aas.registry.model.KeyTypes;
 import org.eclipse.tractusx.semantics.aas.registry.model.LangStringTextType;
 import org.eclipse.tractusx.semantics.aas.registry.model.Reference;
 import org.eclipse.tractusx.semantics.aas.registry.model.ReferenceTypes;
@@ -30,10 +31,12 @@ import org.eclipse.tractusx.semantics.aas.registry.model.SubmodelDescriptor;
 import org.eclipse.tractusx.semantics.registry.model.Submodel;
 import org.eclipse.tractusx.semantics.registry.model.SubmodelDescription;
 import org.eclipse.tractusx.semantics.registry.model.SubmodelEndpoint;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
@@ -114,9 +117,19 @@ public interface SubmodelMapper {
         Reference reference = new Reference();
         reference.setType( ReferenceTypes.EXTERNALREFERENCE );
         Key key = new Key();
+        key.setType( KeyTypes.SUBMODEL );
         key.setValue( semanticId );
+        reference.setKeys( List.of(key) );
         return reference;
     }
 
+   @AfterMapping
+   default Submodel mapSemanticIds( SubmodelDescriptor apiDto, @MappingTarget Submodel submodel){
+      if(apiDto.getSemanticId().getKeys()!=null){
+         return submodel.withSemanticId( apiDto.getSemanticId().getKeys().get( 0 ).getValue() );
+      }else{
+         return submodel.withSemanticId( "" );
+      }
+   }
 
 }
