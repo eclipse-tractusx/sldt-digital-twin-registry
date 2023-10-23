081feec46b745c65414acbfb52a783a5e9cabade
RHJ5FE <shijin.raj@de.bosch.com>
2023-10-23 13:18:56 +0200

    initail revision

--- backend/src/test/java/org/eclipse/tractusx/semantics/registry/AbstractAssetAdministrationShellApi.java
+++ backend/src/test/java/org/eclipse/tractusx/semantics/registry/AbstractAssetAdministrationShellApi.java
@@ -45,12 +45,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @EnableConfigurationProperties( RegistryProperties.class)
 public abstract class AbstractAssetAdministrationShellApi {
 
-    protected static final String SHELL_BASE_PATH = "/api/v3.0/shell-descriptors";
-    protected static final String SINGLE_SHELL_BASE_PATH = "/api/v3.0/shell-descriptors/{aasIdentifier}";
-    protected static final String LOOKUP_SHELL_BASE_PATH = "/api/v3.0/lookup/shells";
-    protected static final String SINGLE_LOOKUP_SHELL_BASE_PATH = "/api/v3.0/lookup/shells/{aasIdentifier}";
-    protected static final String SUB_MODEL_BASE_PATH = "/api/v3.0/shell-descriptors/{aasIdentifier}/submodel-descriptors";
-    protected static final String SINGLE_SUB_MODEL_BASE_PATH = "/api/v3.0/shell-descriptors/{aasIdentifier}/submodel-descriptors/{submodelIdentifier}";
+    protected static final String SHELL_BASE_PATH = "/dec-registry/api/v3.0/shell-descriptors";
+    protected static final String SINGLE_SHELL_BASE_PATH = "/dec-registry/api/v3.0/shell-descriptors/{aasIdentifier}";
+    protected static final String LOOKUP_SHELL_BASE_PATH = "/dec-registry/api/v3.0/lookup/shells";
+    protected static final String SINGLE_LOOKUP_SHELL_BASE_PATH = "/dec-registry/api/v3.0/lookup/shells/{aasIdentifier}";
+    protected static final String SUB_MODEL_BASE_PATH = "/dec-registry/api/v3.0/shell-descriptors/{aasIdentifier}/submodel-descriptors";
+    protected static final String SINGLE_SUB_MODEL_BASE_PATH = "/dec-registry/api/v3.0/shell-descriptors/{aasIdentifier}/submodel-descriptors/{submodelIdentifier}";
 
     protected static final String EXTERNAL_SUBJECT_ID_HEADER = "Edc-Bpn";
 
