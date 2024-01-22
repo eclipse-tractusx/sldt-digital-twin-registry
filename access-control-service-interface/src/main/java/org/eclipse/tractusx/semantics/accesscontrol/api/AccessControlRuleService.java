2a94058a6694d2ff2c6928ec19feeaf62dc1a9d1
Istvan Zoltan Nagy <NIY1FE@bosch.com>
2024-01-22 17:18:35 +0100

    Extend DTR APIs for Access management - Adds new module to provide service contract for access control rules

---
+++ access-control-service-interface/src/main/java/org/eclipse/tractusx/semantics/accesscontrol/api/AccessControlRuleService.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Robert Bosch Manufacturing Solutions GmbH and others
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ ******************************************************************************/
+
+package org.eclipse.tractusx.semantics.accesscontrol.api;
+
+import java.util.Set;
+
+import org.eclipse.tractusx.semantics.accesscontrol.api.exception.DenyAccessException;
+import org.eclipse.tractusx.semantics.accesscontrol.api.model.AccessRule;
+import org.eclipse.tractusx.semantics.accesscontrol.api.model.ShellVisibilityCriteria;
+import org.eclipse.tractusx.semantics.accesscontrol.api.model.SpecificAssetId;
+
+public interface AccessControlRuleService {
+
+   Set<SpecificAssetId> filterValidSpecificAssetIdsForLookup( Set<SpecificAssetId> specificAssetIds, String bpn ) throws DenyAccessException;
+
+   ShellVisibilityCriteria fetchVisibilityCriteriaForShell( Set<SpecificAssetId> specificAssetIds, String bpn ) throws DenyAccessException;
+
+   Set<AccessRule> fetchApplicableRulesForPartner( String bpn ) throws DenyAccessException;
+
+}
