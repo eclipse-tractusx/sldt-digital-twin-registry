c85d70ffd1ba8ca23c1720795976950e23fe3e99
Istvan Zoltan Nagy <NIY1FE@bosch.com>
2024-03-27 08:16:07 +0100

    EDC extension - Implements DTR authorization client - Update dependencies
    Updates #352
    
    Signed-off-by: Istvan Zoltan Nagy <NIY1FE@bosch.com>

---
+++ libraries/edc-extension/src/main/java/org/eclipse/tractusx/semantics/edc/dataplane/http/accesscontrol/client/HttpAccessVerificationClient.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Robert Bosch Manufacturing Solutions GmbH and others
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.eclipse.tractusx.semantics.edc.dataplane.http.accesscontrol.client;
+
+import java.util.Map;
+
+import org.eclipse.tractusx.semantics.edc.dataplane.http.accesscontrol.AccessControlServiceException;
+
+public interface HttpAccessVerificationClient {
+
+   boolean isAspectModelCall( String url );
+
+   boolean shouldAllowAccess(
+         final String requestedUriPath, final String requestedQueryString, Map<String, String> additionalHeaders ) throws AccessControlServiceException;
+
+}
