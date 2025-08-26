817a71e14612be35b525f803d50aa3933f9ac1ba
Philippe Heiler <philippe.heiler@iese.fraunhofer.de>
2025-08-26 14:38:26 +0200

    Fix: Added missing sql alias

--- backend/src/main/java/org/eclipse/tractusx/semantics/registry/repository/ShellRepository.java
+++ backend/src/main/java/org/eclipse/tractusx/semantics/registry/repository/ShellRepository.java
@@ -129,7 +129,7 @@ public interface ShellRepository extends JpaRepository<Shell, UUID>, JpaSpecific
                            )
                      )
                  )
-           )
+           ) AS shell_count
         """,
            nativeQuery = true
    )
